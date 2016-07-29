a46567bdc5cd987ddb897de3827452217e07a5c6
duanSaiSai <duanSaiSai@172.22.107.222>
2016-07-29 21:33:51 +0800

    ACm

---
+++ src/first.java
@@ -0,0 +1,18 @@
+/** 
+ * @author Michael2397 2692613726@qq.com: 
+ * @version 创建时间：2016年7月15日 下午2:44:39 
+ * 类说明 
+ */
+public class first {
+
+	public static void main(String[] args) {
+		int a = 10;
+		a = 11;
+		int b = 12;
+		b = 13;
+		add(a,b);
+	}
+    public static void add(int a,int b){
+    	System.out.println("a+b="+(a+b));
+    }
+}
